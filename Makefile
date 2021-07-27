f17203d0daa4d4d9ed969d1de1cae192bf089dc1
Mustafif <mustafif0929@gmail.com>
2021-07-27 12:57:48 -0400

    updated content

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
 build: 
-
+	make mdbook
+	make tex 
+	make zip 
+	make git
+mdbook: 
+	mdbook build
 zip: 
 	python3 zip_release.py
 git: 
@@ -7,7 +12,8 @@ git:
 	git commit -m "updated content"
 	git push
 tex: 
-	cd tex && pdflatex main x 2
+	cd tex && pdflatex TheGistOfRust x 2
+	mv tex/TheGistOfRust.pdf docs/
 plan-tex:
 	cd plan && pdflatex plan.tex x 2
 	mv plan/plan.pdf docs/
--- zip_release.py
+++ zip_release.py
@@ -14,12 +14,15 @@ book = "TheGistOfRust"
 all_ = f'{book}_all.zip'
 web = f'{book}_web.zip'
 pdf = f'{book}_pdf.zip'
+cs = f'{book}_cs.zip'
 
 
 # Paths to all zip formats
 zip_all = zipfile.ZipFile(f'releases/{all_}', 'w', zipfile.ZIP_DEFLATED)
 zip_web = zipfile.ZipFile(f'releases/{web}', 'w', zipfile.ZIP_DEFLATED)
 zip_pdf = zipfile.ZipFile(f'releases/{pdf}', 'w', zipfile.ZIP_DEFLATED)
+zip_cs = zipfile.ZipFile(f'releases/{cs}', 'w', zipfile.ZIP_DEFLATED)
+
 
 # all directories for TheGistOfRust_all.zip
 zipdir('docs/TheGistOfRust.pdf', zip_all)
@@ -27,17 +30,24 @@ zipdir('tex/', zip_all)
 zipdir('book/', zip_all)
 zipdir('src/', zip_all)
 zipdir('book.toml', zip_all)
+zipdir('cheatsheet/', zip_all)
+zipdir('images/', zip_all)
 
 # all directories for TheGistOfRust_web.zip
 zipdir('src/', zip_web)
 zipdir('book/', zip_web)
 zipdir('book.toml', zip_web)
+zipdir('images/', zip_web)
 
 # all directories for TheGistOfRust_pdf.zip
 zipdir('docs/TheGistOfRust.pdf', zip_pdf)
 zipdir('tex/', zip_pdf)
+zipdir('images/', zip_pdf)
 
+# all directories for TheGistOfRust_cs.pdf
+zipdir('cheatsheet/', zip_cs)
 
 zip_all.close()
 zip_web.close()
 zip_pdf.close()
+zip_cs.close()
